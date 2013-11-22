fe6ffdd23d5d9174d170c46269c938ca4c43a338
tomykaira <tomykaira@gmail.com>
2013-11-22 11:30:08 +0900

    Add error handling to charge.php

--- charge.php
+++ charge.php
@@ -10,12 +10,52 @@ $amount = $_POST['amount'];
 $token = $_POST['webpay-token'];
 
 $webpay = new WebPay(SECRET_KEY);
-$result = $webpay->charges->create(array(
-   "amount" => intval($amount, 10),
-   "currency" => "jpy",
-   "card" => $token,
-   "description" => "PHP からのアイテムの購入"
-));
+
+try {
+    $result = $webpay->charges->create(array(
+       "amount" => intval($amount, 10),
+       "currency" => "jpy",
+       "card" => $token,
+       "description" => "PHP からのアイテムの購入"
+    ));
+} catch (\WebPay\Exception\CardException $e) {
+    // カードが拒否された場合
+    print("CardException\n");
+    print('Status is:' . $e->getStatus() . "\n");
+    print('Type is:' . $e->getType() . "\n");
+    print('Code is:' . $e->getCardErrorCode() . "\n");
+    print('Param is:' . $e->getParam() . "\n");
+    print('Message is:' . $e->getMessage() . "\n");
+    exit('Error');
+} catch (\WebPay\Exception\InvalidRequestException $e) {
+    // リクエストで指定したパラメータが不正な場合
+    print("InvalidRequestException\n");
+    print('Param is:' . $e->getParam() . "\n");
+    print('Message is:' . $e->getMessage() . "\n");
+    exit('Error');
+} catch (\WebPay\Exception\AuthenticationException $e) {
+    // 認証に失敗した場合
+    print("AuthenticationException\n");
+    print('Param is:' . $e->getParam() . "\n");
+    print('Message is:' . $e->getMessage() . "\n");
+    exit('Error');
+} catch (\WebPay\Exception\APIConnectionException $e) {
+    // APIへの接続エラーが起きた場合
+    print("APIConnectionException\n");
+    print('Param is:' . $e->getParam() . "\n");
+    print('Message is:' . $e->getMessage() . "\n");
+    exit('Error');
+} catch (\WebPay\Exception\APIException $e) {
+    // WebPayのサーバでエラーが起きた場合
+    print("APIException\n");
+    print('Message is:' . $e->getMessage() . "\n");
+    exit('Error');
+} catch (Exception $e) {
+    // WebPayとは関係ない例外の場合
+    print("Unexpected exception\n");
+    print('Message is:' . $e->getMessage() . "\n");
+    exit('Error');
+}
 
 // 処理終了後、 https://webpay.jp/test/charges で課金が発生したことが分かります。
 ?>
